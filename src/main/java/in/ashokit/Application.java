7bc0a5e00e15e413ed41a933ac41eb0c75848e6f
AkhilReddyYalla <yallaakhilreddy2020@gmail.com>
2024-05-20 21:57:51 +0530

    changes commited

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -2,8 +2,10 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.akhil.project", "com.capGem"})
 public class Application {
 
 	public static void main(String[] args) {
